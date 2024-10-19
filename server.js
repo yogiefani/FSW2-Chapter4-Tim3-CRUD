fba1a6c67fef01c6abcaf83555e0d136c1254f0c
haikalnuril <haikal.nuril23@gmail.com>
2024-10-19 22:57:44 +0700

    feat: landing page

--- server.js
+++ server.js
@@ -17,14 +17,19 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(morgan());
 
+app.use(express.static(`${__dirname}/public`));
+
+// View Engine
+app.set("view engine", "ejs");
+app.use(EJSLayouts);
+app.set("layout", "layouts/template");
+
 // Health Check
 app.get("/", async (req, res) => {
     try {
-        res.status(200).json({
-            status: "Succeed",
-            message: "Ping successfully",
-            isSuccess: true,
-        });
+        res.render("landingPage",{
+            title: "WorkSync.",
+        })
     } catch (error) {
         res.status(500).json({
             status: "Failed",
@@ -34,13 +39,6 @@ app.get("/", async (req, res) => {
         });
     }
 });
-
-app.use(express.static(`${__dirname}/public`));
-
-// View Engine
-app.set("view engine", "ejs");
-app.use(EJSLayouts);
-app.set("layout", "layouts/template");
 
 // Configure method-override to use _method query parameter
 app.use(methodOverride('_method'));
