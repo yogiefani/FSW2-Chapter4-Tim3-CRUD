e6e3669bb812f062907199ff52c02f9b244000f7
AbdiPranawa <asaturaardana@gmail.com>
2024-10-20 05:23:33 +0700

    add role controller

---
+++ controllers/roleController.js
@@ -0,0 +1,83 @@
+const { role } = require("../models");
+
+const getAllRoles = async (req, res) => {
+  try {
+    const roles = await role.findAll();
+    res.status(200).json({
+      status: "Succeed",
+      message: "Get all roles successfully",
+      isSuccess: true,
+      data: roles,
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "Failed",
+      message: "Internal server error",
+      isSuccess: false,
+      error: err.message,
+    });
+  }
+};
+
+async function getRoleById(req, res) {
+  const id = req.params.id;
+  try {
+    const role = await role.findByPk(id);
+    if (!role) {
+      return res.status(404).json({
+        status: "Failed",
+        message: "Can't find spesific id role",
+        isSuccess: false,
+        data: null,
+      });
+    }
+    res.status(200).json({
+      status: "Success",
+      message: "Successfully obtained role data",
+      isSuccess: true,
+      data: { role },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "Failed",
+      message: "Failed to get role data",
+      isSuccess: false,
+      data: null,
+      error: error.message,
+    });
+  }
+}
+
+async function deleteRole(req, res) {
+  const id = req.params.id;
+  try {
+    const role = await role.findByPk(id);
+    if (!role) {
+      return res.status(404).json({
+        status: "Failed",
+        message: "Can't find spesific id role",
+        isSuccess: false,
+        data: null,
+      });
+    }
+
+    await role.destroy();
+
+    res.status(200).json({
+      status: "Success",
+      message: "Successfully delete role data",
+      isSuccess: true,
+      data: { role },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "Failed",
+      message: "Failed to delete role data",
+      isSuccess: false,
+      data: null,
+      error: error.message,
+    });
+  }
+}
+
+module.exports = { getAllRoles, getRoleById, deleteRole };
